526a0b9fff945fd1370fef376d7d913b52e4d376
deepakmallah <deepak.mallah@gmail.com>
2017-11-22 15:20:48 +0530

    Showing tweets from API

--- src/app/modules/feed/FeedContainer.jsx
+++ src/app/modules/feed/FeedContainer.jsx
@@ -14,38 +14,7 @@ class FeedComponent extends Component {
     super(props);
     this.getFeed = this.getFeed.bind(this);
     this.state = {
-      tweets: [
-        { text: 'WebPack', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/webpack.svg' },
-        { text: 'React', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/react.svg' },
-        {
-          text: 'React Router',
-          imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/react-router.svg',
-        },
-        { text: 'Babel', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/babel.svg' },
-        { text: 'PostCSS', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/postcss.svg' },
-        { text: 'ESLint', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/eslint.svg' },
-        { text: 'Jest', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/jest.svg' },
-        {
-          text: 'Ant Design',
-          imgUrl: 'https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg',
-        },
-        { text: 'Babel', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/babel.svg' },
-        { text: 'PostCSS', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/postcss.svg' },
-        { text: 'ESLint', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/eslint.svg' },
-        { text: 'Jest', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/jest.svg' },
-        {
-          text: 'Ant Design',
-          imgUrl: 'https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg',
-        },
-        { text: 'Babel', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/babel.svg' },
-        { text: 'PostCSS', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/postcss.svg' },
-        { text: 'ESLint', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/eslint.svg' },
-        { text: 'Jest', imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/jest.svg' },
-        {
-          text: 'Ant Design',
-          imgUrl: 'https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg',
-        }
-      ]
+      tweets: []
     };
 
     this.handleScroll = this.handleScroll.bind(this);
@@ -56,6 +25,10 @@ class FeedComponent extends Component {
       getTweets()
         .then(response => {
           console.log("getTweets response", response);
+          if(response.body.length){
+            if(!this.state.tweets.length) this.state.tweets = response.body;
+            this.setState({tweets: this.state.tweets});
+          }
         })
         .catch(error => console.log(error));
     }
@@ -85,7 +58,7 @@ class FeedComponent extends Component {
     if((fixed.bottom - scroll.bottom) <= 0){
       console.log("reached")
       this.state.tweets.push({ text: 'pushed '+this.state.tweets.length, imgUrl: 'https://s3-us-west-2.amazonaws.com/svgporn.com/logos/webpack.svg' });
-      this.setState({emails: this.state.tweets});
+      this.setState({tweets: this.state.tweets});
     }
 
     console.log('the scroll things', fixed.bottom,scroll.bottom)
@@ -94,18 +67,16 @@ class FeedComponent extends Component {
   render() {
     return (
       <div>
-        <div className="infinite-scroll-example">
-          <div className="infinite-scroll-example__scrollable-parent" style={divStyle} ref="scrollDiv">
-            {this.state.tweets.map((tweet, index) =>
-              <li key={index} className="image-list__item">
-                <div>{tweet.text}</div>
-              </li>
-            )}
-            <div ref="targetDiv"></div>
-          </div>
-          <div ref="targetDiv1"></div>
-          <p className="infinite-scroll-example__arrow" />
+        <div className="scroll-container" style={divStyle} ref="scrollDiv">
+          {this.state.tweets.map((tweet, index) =>
+            <li key={tweet.id} className="image-list__item">
+              <div>{tweet.text}</div>
+            </li>
+          )}
+          <div ref="targetDiv"></div>
         </div>
+        <div ref="targetDiv1"></div>
+        <p className="infinite-scroll-example__arrow" />
       </div>
     );
   }
