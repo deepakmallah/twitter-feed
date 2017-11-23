62aad5eb557eedceadfb91e910733f3321e269a2
deepakmallah <deepak.mallah@gmail.com>
2017-11-23 13:19:37 +0530

    Removed extra link from header

--- src/app/modules/root/components/Header/Header.jsx
+++ src/app/modules/root/components/Header/Header.jsx
@@ -25,9 +25,9 @@ class HeaderComponent extends Component {
           <Menu.Item key="1">
             <Link to="/">Home</Link>
           </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/feed">Feed</Link>
-          </Menu.Item>
+          {/*<Menu.Item key="2">*/}
+            {/*<Link to="/feed">Feed</Link>*/}
+          {/*</Menu.Item>*/}
         </Menu>
       </Header>
     );
